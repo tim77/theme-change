35fe6b2c056c10bec5ee11b96b04ea893f87fec7
Artem Polishchuk <tim77@users.noreply.github.com>
2019-06-12 12:33:50 +0300

    Update theme-switcher-auto.sh

--- theme-switcher-auto.sh
+++ theme-switcher-auto.sh
@@ -4,15 +4,15 @@ time=$(date +"%H:%M")
 
 # List and set your gnome-terminal profiles:
 # gsettings get org.gnome.Terminal.ProfilesList list
-terminal_dark=88173e30-df6e-4442-b012-4e1119c7385f
-terminal_light=b4bd0ffd-117e-4778-82ef-da4ccdf4cb2c
+terminal_dark='88173e30-df6e-4442-b012-4e1119c7385f'
+terminal_light='b4bd0ffd-117e-4778-82ef-da4ccdf4cb2c'
 
 if [[ $time > '20:00' ]] || [[ $time < '06:00' ]]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-    gsettings set org.gnome.Terminal.ProfilesList default '$terminal_dark'
+    gsettings set org.gnome.Terminal.ProfilesList default $terminal_dark
     exit 0
 else
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
-    gsettings set org.gnome.Terminal.ProfilesList default '$terminal_light'
+    gsettings set org.gnome.Terminal.ProfilesList default $terminal_light
     exit 0
 fi
